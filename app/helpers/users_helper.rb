e464dd004ccbd38c3ee42ab42ef535dd4ff36c09
ReiShibuya <dir_en_grey_shibuzo@yahoo.co.jp>
2019-11-07 03:10:46 +0900

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def gravatar_for(user, size: 80)
+    gravator_id = Digest::MD5::hexdigest(user.email.downcase)
+    # size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravator_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
